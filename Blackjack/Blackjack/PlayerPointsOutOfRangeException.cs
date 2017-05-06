31a4d73b968fd026d230c12af5980488e9e03c77
Simone Fuoco <simone.fuoco@edu.itspiemonte.it>
2017-05-06 16:41:23 +0200

    PlayerPointsOutOfRangeException

---
+++ Blackjack/Blackjack/PlayerPointsOutOfRangeException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    [Serializable]
+    public class PlayerPointsOutOfRangeException : ApplicationException
+    {
+        public PlayerPointsOutOfRangeException() : this("Player Out") {}
+
+        public PlayerPointsOutOfRangeException(string message) : base(message) {}
+
+        public PlayerPointsOutOfRangeException(string message, Exception innerException)
+            : base(message, innerException) {}
+
+        public PlayerPointsOutOfRangeException(SerializationInfo info, StreamingContext context)
+            : base(info, context) {}
+    }
+}
