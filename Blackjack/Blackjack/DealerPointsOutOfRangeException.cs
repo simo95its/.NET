4c5f5cd8532d7541397406558ee79b22198abc4b
Simone Fuoco <simone.fuoco@edu.itspiemonte.it>
2017-05-06 16:44:09 +0200

    DealerPointsOutOfRangeException

---
+++ Blackjack/Blackjack/DealerPointsOutOfRangeException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    [Serializable]
+    public class DealerPointsOutOfRangeException : ApplicationException
+    {
+        public DealerPointsOutOfRangeException() : this("Player Out") { }
+
+        public DealerPointsOutOfRangeException(string message) : base(message) { }
+
+        public DealerPointsOutOfRangeException(string message, Exception innerException)
+            : base(message, innerException) { }
+
+        public DealerPointsOutOfRangeException(SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
+    }
+}
