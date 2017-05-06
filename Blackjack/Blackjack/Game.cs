802b15a4a59142b0c2e44ae6db21fb594cac0d30
Simone Fuoco <simone.fuoco@edu.itspiemonte.it>
2017-05-06 19:05:26 +0200

    Game

---
+++ Blackjack/Blackjack/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Blackjack
+{
+    [Serializable]
+    public class Game
+    {
+        [XmlAttribute("started")]
+        public DateTime DateStarted { get; set; }
+        [XmlAttribute("end")]
+        public DateTime DateEnd { get; set; }
+        [XmlElement("playerPoints")]
+        public int PlayerPoints { get; set; }
+        [XmlElement("dealerPoints")]
+        public int DealerPoints { get; set; }
+        [XmlElement("winner")]
+        public bool Winner { get; set; }
+    }
+}
