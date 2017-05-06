96f8918f29ceabf8c06b5bedc746bf2deb6196c4
Simone Fuoco <simone.fuoco@edu.itspiemonte.it>
2017-05-06 14:44:04 +0200

    Upgrade BlackjackForm

--- Blackjack/Blackjack/BlackjackForm.Designer.cs
+++ Blackjack/Blackjack/BlackjackForm.Designer.cs
@@ -28,20 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.DealerLabel = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.SuspendLayout();
+            // 
+            // DealerLabel
+            // 
+            this.DealerLabel.AutoSize = true;
+            this.DealerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DealerLabel.Location = new System.Drawing.Point(13, 13);
+            this.DealerLabel.Name = "DealerLabel";
+            this.DealerLabel.Size = new System.Drawing.Size(75, 25);
+            this.DealerLabel.TabIndex = 0;
+            this.DealerLabel.Text = "Dealer";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(18, 42);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 100);
+            this.flowLayoutPanel1.TabIndex = 1;
             // 
             // BlackjackForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
             this.ClientSize = new System.Drawing.Size(848, 496);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.DealerLabel);
             this.Name = "BlackjackForm";
             this.Text = "Blackjack";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label DealerLabel;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
 
