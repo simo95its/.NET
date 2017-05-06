9fcff0c5893ef1b0521024f6397b5734f6ff2429
Simone Fuoco <simone.fuoco@edu.itspiemonte.it>
2017-05-06 19:08:22 +0200

    Games

---
+++ Blackjack/Blackjack/Games.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Blackjack
+{
+    [Serializable]
+    [XmlRoot("games")]
+    public class Games
+    {
+        [XmlElement("game")]
+        public Game[] GameList { get; set; }
+    }
+}
