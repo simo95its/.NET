ccf13f54772b424c8311198bb731479b2a05ef82
Simone Fuoco <simone.fuoco@edu.itspiemonte.it>
2017-05-06 14:56:18 +0200

    Card

---
+++ Blackjack/Blackjack/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    public class Card
+    {
+        public Seed Seed { get; set; }
+        public Value Value { get; set; }
+
+        public Card(Seed seed, Value value)
+        {
+            Seed = seed;
+            Value = value;
+        }
+    }
+}
